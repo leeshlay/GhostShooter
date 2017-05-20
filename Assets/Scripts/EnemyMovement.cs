ea2442694026c40322473083b6a406e1cdca6d08
leeshlay <kijak.shia@gmail.com>
2017-05-20 22:30:09 +0200

    Add enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform player;
+	NavMeshAgent nav;
+
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination(player.position);
+	}
+
+	/*void Update()
+	{
+        Debug.Log("in update");
+		RaycastHit hit;
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+				nav.SetDestination(hit.point);
+
+		}
+	}*/
+}
