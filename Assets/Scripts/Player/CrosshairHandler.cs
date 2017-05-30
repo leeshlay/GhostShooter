b72cb5a517e69a7d4cb39b8c46e5c06268438548
Piotr Martyna <piotrek722@gmail.com>
2017-05-30 20:35:32 +0200

    crosshair

---
+++ Assets/Scripts/Player/CrosshairHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairHandler: MonoBehaviour {
+
+    public void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    public void Update()
+    {
+        transform.position = Input.mousePosition;
+
+        if (Input.GetMouseButtonDown(0)){
+            Debug.Log("Pressed left click.");
+        }
+
+    }
+
+}
